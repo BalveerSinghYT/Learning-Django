c8057a5da0881ca7f8ae74f1dca9f124609ec4cc
Balveer Singh <balveersinghyt@gmail.com>
2020-12-05 22:35:21 +0530

    rename of Images Complete

--- home/models.py
+++ home/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+import os
 # Create your models here.
+
+def photo_path(instance, filename):
+    basefilename, file_extension= os.path.splitext(filename)
+    print(basefilename)
+    return 'FaceImages/{basename}{ext}'.format(basename= instance.name, ext= file_extension)
 
 class registeration(models.Model):
     name = models.CharField(max_length=30)
     roll_no = models.IntegerField()
-    img = models.ImageField(upload_to = 'FaceImages', verbose_name="Profile Picture")
+    img = models.ImageField(upload_to = photo_path, verbose_name="Profile Picture"    )
     email = models.EmailField()
     department = models.CharField(max_length=4)
         
     def __str__(self):
         return self.name
+
